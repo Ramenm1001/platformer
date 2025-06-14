d74018412b1d3fcdd1145ff18434b44f7c43c065
Роббо <ramenm1001@gmail.com>
2025-06-14 14:23:46 +1000

    start

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import pygame
+
+win = pygame.display.set_mode((500, 500))
+run = True
+player = None
+platforms = []
+while run:
+    for eve in pygame.event.get():
+        if eve.type == pygame.QUIT:
+            run = False
+
+    win.fill((0,0,0))
+    for platform in platforms:
+        platform.update()
+    pygame.display.update()
+pygame.quit()

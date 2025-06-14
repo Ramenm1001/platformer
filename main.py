edb1e81e5dc1deb0e154140872d880fdd8dea0b5
Роббо <ramenm1001@gmail.com>
2025-06-14 14:47:34 +1000

    paint

--- main.py
+++ main.py
@@ -4,13 +4,27 @@ win = pygame.display.set_mode((500, 500))
 run = True
 player = None
 platforms = []
+platforms_draw = []
+drawing = False
+paint = 100
 while run:
     for eve in pygame.event.get():
         if eve.type == pygame.QUIT:
             run = False
+        if eve.type == pygame.MOUSEBUTTONDOWN:
+            drawing = True
+        # if eve.type
+    mouse = pygame.mouse.get_pos() # (150, 125)
+    if drawing and paint > 0:
+        # ??????
+        platforms_draw.append(?????)
+        # paint ???
 
-    win.fill((0,0,0))
+
+    win.fill((0, 0, 0))
     for platform in platforms:
+        platform.update()
+    for platform in platforms_draw:
         platform.update()
     pygame.display.update()
 pygame.quit()
